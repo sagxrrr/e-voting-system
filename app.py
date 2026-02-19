d62243badaea2409e6aa21922a4ce5491ddcc492
Sagar Pol <polsagar880@gmail.com>
2026-02-19 14:42:28 +0530

    updated port for render

--- app.py
+++ app.py
@@ -493,4 +493,4 @@ def show_election():
 # ================END==================
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0",port=int(os.environ.get("PORT", 5000)))
